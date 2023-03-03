41ac9ba20307c94343b1f46be983d483cd65f71b
deividas939 <121436093+deividas939@users.noreply.github.com>
2023-03-03 15:30:37 +0200

    Jokes-on-you app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+
+const API_URL = 'https://v2.jokeapi.dev/joke/Programming?amount=10';
 
 function App() {
+  const [jokes, setJokes] = useState('');
+
+  useEffect(() => {
+    fetch(API_URL)
+    .then(res => res.json())
+    .then(data => setJokes(data.value.jokes));
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="box">
+      <h1>Jokes On You Generator</h1>
+      <p>{jokes}</p>
+      <button>Get New Joke!</button>
     </div>
   );
 }
